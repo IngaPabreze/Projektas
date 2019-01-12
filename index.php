a61038e15138bb1958c71f9e123d8fc99b1750b5
IngaPabreze <inga.pabreze@gmail.com>
2019-01-12 15:25:52 +0200

    save

--- index.php
+++ index.php
@@ -38,13 +38,9 @@
 
             function drawArticle(articleData){
                 var rowDiv = $("<div>").addClass("row");
-
                 var article = $("<article>").addClass("scol12").addClass(articleData.mColClass);
-
                 var articleBox = $("<div>").addClass("box");
-
                 var nr = articleData.articleNr;
-
                 var fullArticle = $(article).append($(articleBox).append('<h1 class="artitle">'+articleData.title+'</h1>').append('<img class="firstImg" src="'+articleData.url+'" style="float: left">').append('<p>'+articleData.firstParagraph+'</p>').append($('<div class="flex"><button id="button'+nr+'">Read more</button><button>Video</button><button id="button2'+nr+'">Gallery</button></div>')));
                 $(".rowDiv").append(fullArticle);
                 $('#button'+nr+'').click(function(){
